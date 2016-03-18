96fb9b06b9c176c7a1eb728e4467f098a31dc71d
akpabioig <1414987@rgu.ac.uk>
2016-03-18 14:51:37 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,9 +7,9 @@
  */
 
 
-define('DBSERVER', 'localhost');
-define('DBUSER', 'root');
-define('DBPASSWORD', '');
+define('DBSERVER', 'us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms');
+define('DBUSER', 'bee886bc8793e7');
+define('DBPASSWORD', '362289e3');
 define('DBNAME', 'inventoryms');
 
 $db = mysqli_connect(DBSERVER, DBUSER, DBPASSWORD, DBNAME);
