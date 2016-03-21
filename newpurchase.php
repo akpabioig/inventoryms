9c9df4afededaee5f28e79f3ccc430bd8544aa61
akpabioignatius <1414987@rgu.ac.uk>
2016-03-21 22:50:15 +0000

    Customer Edit

--- newpurchase.php
+++ newpurchase.php
@@ -64,8 +64,8 @@
                             if(mysqli_num_rows($result) == 1 || mysqli_num_rows($result) >1){
 
                                 while($row = $result -> fetch_array()){
-                                    echo '<option value="'.$row['suppliername'].'" >';
-                                    echo $row['supplierid'];
+                                    echo '<option value="'.$row['supplierid'].'" >';
+                                    echo $row['suppliername'];
                                     echo '</option>';
                                 }
                             }else{
