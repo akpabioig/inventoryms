3892493cb1516393817ca1375031a74695a89f97
akpabioignatius <1414987@rgu.ac.uk>
2016-03-22 11:59:31 +0000

    Customer Edit

---
+++ loginphp.php
@@ -0,0 +1,29 @@
+<?php
+
+$db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3',array(PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+function SignIn()
+{
+    session_start();
+    if (!empty($_POST['username']))
+    {
+        $query = mysql_query("SELECT * FROM login where username = '$_POST[username]' AND pass = '$_POST[password]'") or die(mysql_error());
+        $row = mysql_fetch_array($query) or die(mysql_error());
+        if(!empty($row['username']) AND !empty($row['password']))
+        {
+            $_SESSION['userName'] = $row['password']; echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
+        }
+        else { echo "PLEASE ENTER THE CORRECT ID AND PASSWORD";
+        }
+
+    }
+}
+if(isset($_POST['submit']))
+{
+    SignIn();
+}
+?>
