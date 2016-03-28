13fe2d45adcc38dfbdbf946b1012f95bf386f0c1
akpabioignatius <1414987@rgu.ac.uk>
2016-03-28 13:50:41 +0100

    Login Coding

---
+++ createuser.php
@@ -0,0 +1,74 @@
+<?php include('connection.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> LOGIN PAGE </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href = "styling.css"/>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+<div style="margin-left:70px">
+    <header id = "headlogin">
+        <h1> RGU Oil Services </h1>
+        <h2> Inventory Management Made Easy </h2>
+    </header>
+    <section id = "content">
+        <div id = "log">
+            <h2> RGU Oil Services IMS Login</h2>
+            <h4>Create User</h4>
+            <form method="POST" action="usercreation.php">
+                <table>
+                    <tr>
+                        <td label for = firstname> FIRST NAME : </td>
+                    </tr>
+                    <tr>
+                        <td> <input type="text" id="firstname" name="firstname" value="" class = "formfiel" rows = 1 cols = 40></td>
+                    </tr>
+                    <tr>
+                        <td label for = middlename> MIDDLE NAME : </td>
+                    </tr>
+                    <tr>
+                        <td> <input type="text" id="middlename" name="middlename" value="" class = "formfiel" rows = 1 cols = 40></td>
+                    </tr>
+                    <tr>
+                        <td label for = middlename> LAST NAME : </td>
+                    </tr>
+                    <tr>
+                        <td> <input type="text" id="middlename" name="middlename" value="" class = "formfiel" rows = 1 cols = 40></td>
+                    </tr>
+                    <tr>
+                        <td label for = position> POSITION : </td>
+                    </tr>
+                    <tr>
+                        <td> <input type="text" id="position" name="position" value="" class = "formfiel" rows = 1 cols = 40></td>
+                    </tr>
+                    <tr>
+                        <td label for="username"> <a id = "hash">*</a> UserName : </td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" id="myusername" name="myusername" value="" class = "formfiel" rows = 1 cols = 40></td>
+                    </tr>
+                    <tr>
+                        <td label for="pass"> <a id = "hash">*</a> Password : </td>
+                    </tr>
+                    <tr>
+                        <td><input type="password" id="mypassword" name="mypassword" value="" class = "formfiel" rows = 1 cols = 40></td>
+                    </tr>
+                </table>
+                <h5><a href="#"> Forgot Password ? </a></h5>
+                <p id = "signup"><input type = "submit" name = "submit" value = "REGISTER USER"></p>
+            </form>
+        </div>
+    </section>
+</div>
+<footer id= "footlogin">
+    <p>&copy; Akpabio Ignatius, 2016</p>
+</footer>
+</body>
+</html>
