440d138c0beb69e7d7687e68a3fdff3aaa9f29f9
akpabioignatius <1414987@rgu.ac.uk>
2016-03-29 16:56:47 +0100

    Alert successful and redirect

--- location.php
+++ location.php
@@ -153,23 +153,37 @@ include('connection.php');
                         <th>Product Serial Number </th>
                         <th>Product Name</th>
                         <th>Product Description </th>
-                        <th>Supplier Name</th>
                         <th>Initial Stock Price</th>
                         <th>Wholesale Price</th>
                         <th>Retail Price</th>
                     </tr>
-                    <tr>
-                        <td><input type="text" id = "productid" name= "productid"  value = "" class = "tablefield"></td>
-                        <td><input type="text" id = "productsn" name= "productsn"  value = "" class = "tablefield"></td>
-                        <td><input type="text" id = "productname" name= "productname"  value = "" class = "tablefield"></td>
-                        <td><input type="text" id = "productdesc" name= "productdesc"  value = "" class = "tablefield"></td>
-                        <td><input type="text" id = "suppname" name= "suppname"  value = "" class = "tablefield"></td>
-                        <td><input type="text" id = "isp" name= "isp"  value = "" class = "tablefield"></td>
-                        <td><input type="text" id = "wp" name= "wp"  value = "" class = "tablefield"></td>
-                        <td><input type="text" id = "rp" name= "rp"  value = "" class = "tablefield"></td>
+                        <?php
 
+                        $sql= "SELECT productid, productserialnumber, productname, productdescription, initialstockprice, wholesaleprice, retailprice  FROM addproduct WHERE locationid = 'WH Location A1'";
+                        $result = mysqli_query($db, $sql);
+                        if(mysqli_num_rows($result) == 1 || mysqli_num_rows($result) >1){
+
+                            while($row = $result -> fetch_array()){
+                                echo "
+
+                                <tr>
+                        <td><input type=\"text\" id = \"productid\" name= \"productid\"  value = \"{$row['productid']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"productsn\" name= \"productsn\"  value = \"{$row['productserialnumber']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"productname\" name= \"productname\"  value = \"{$row['productname']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"productdesc\" name= \"productdesc\"  value = \"{$row['productdescription']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"isp\" name= \"isp\"  value = \"{$row['initialstockprice']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"wp\" name= \"wp\"  value = \"{$row['wholesaleprice']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"rp\" name= \"rp\"  value = \"{$row['retailprice']}\" class = \"tablefield\"></td>
                     </tr>
-                        </table>
+
+                                ";
+                            }
+                        }else{
+                            echo '<option> No Result Found </option>';
+                        }
+                        ?>
+
+                    </table>
                 </div>
 
             </div>
