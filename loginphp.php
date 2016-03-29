6dcf51c856c47d59592bab2d378e5df5aea6b7d2
akpabioignatius <1414987@rgu.ac.uk>
2016-03-29 15:04:28 +0100

    Login Coding

--- loginphp.php
+++ loginphp.php
@@ -4,8 +4,8 @@ $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inven
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 
-    $myusername = htmlspecialchars($_POST['myusername']);
-    $mypassword = htmlspecialchars($_POST['mypassword']);
+    echo $myusername = htmlspecialchars($_POST['myusername']);
+    echo $mypassword = htmlspecialchars($_POST['mypassword']);
 //$myusername = mysql_real_escape_string($myusername);
 //$mypassword = mysql_real_escape_string($mypassword);
 /*
