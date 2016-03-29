1bbd478d857b25940c23e9669808e0bbe7aa0062
akpabioignatius <1414987@rgu.ac.uk>
2016-03-29 14:54:23 +0100

    Login Coding

--- loginphp.php
+++ loginphp.php
@@ -3,19 +3,23 @@
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3',array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-$myusername=$_POST['myusername'];
-$mypassword=$_POST['mypassword'];
 
-$myusername = stripslashes($myusername);
-$mypassword = stripslashes($mypassword);
+    $myusername = stripslashes($myusername);
+    $mypassword = stripslashes($mypassword);
 //$myusername = mysql_real_escape_string($myusername);
 //$mypassword = mysql_real_escape_string($mypassword);
 
-try {
-    $sql = "SELECT * FROM login WHERE username = '$myusername' AND password = '$mypassword' LIMIT 1";
-    $sth = $db->query($sql);
-    header("Location: login_success.php");
-} catch(PDOException $e) {
-    echo $e->getMessage();
-}
+    try {
+        $sql = "SELECT * FROM login WHERE username = '$myusername' AND password = '$mypassword' LIMIT 1";
+        $sth = $db->query($sql);
+
+        if($sth ->rowCount() == 1) {
+            header("Location: login_success.php");
+        }else {
+            header("Location: loginphp.php?failed=1");
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+
+    }
 ?>
