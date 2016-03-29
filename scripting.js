09f90e1ff36ce5243f52cbd33f3ea69027e3359d
akpabioignatius <1414987@rgu.ac.uk>
2016-03-29 13:28:57 +0100

    Login Coding

--- scripting.js
+++ scripting.js
@@ -4,12 +4,12 @@ function validateForm(form)
 }
 
 
-function myFunction() {
+function myFunction(form) {
     var pass1 = document.getElementById("password").value;
     var pass2 = document.getElementById("password2").value;
     var ok = true;
     if (pass1 != pass2) {
-        // alert("Passwords Do not match");
+        alert("Passwords Do not match");
         document.getElementById("pass1").style.borderColor = "#E34234";
         document.getElementById("pass2").style.borderColor = "#E34234";
         ok = false;
