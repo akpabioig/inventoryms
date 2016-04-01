e075bf86710d1a6df596d09a2810017603faccfe
Ajibola <jibolso@gmail.com>
2016-04-01 22:18:04 +0100

    divClicked

--- location.php
+++ location.php
@@ -100,7 +100,7 @@ include('connection.php');
         <h2>WareHouse / Yards </h2>
         <p>Select The Location within the Warehouse or Yard you would like to explore its' contents</p>
         <div class = "locbutt">
-            <a id="a" onclick="getid('a');">
+            <a id="a">
                 <img src= "location/a1.jpeg" style{height="150" width="150"} class ="locicons"/>
             </a>
             <a id="a2">
@@ -215,18 +215,11 @@ include('connection.php');
 <script>
 
     // Get the modal
-    var divClicked = "";
+    var divClicked = "a";
     var modal = document.getElementById('myModal');
 
     // Get the button that opens the modal
-
-    var btn;
-    function getid(n){
-        if(n === 'a'){
-            btn = document.getElementById('a');
-            return btn;
-        }
-    }
+    var btn = document.getElementById(divClicked);
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
