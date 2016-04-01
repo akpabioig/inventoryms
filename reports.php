cde6342c0521e31c93851bcdea895b1414fcaf82
akpabioignatius <1414987@rgu.ac.uk>
2016-04-01 01:37:47 +0100

    Alert successful and redirect

--- reports.php
+++ reports.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>ADD PRODUCT</title>
+    <title>REPORTS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href = "styling.css"/>
     <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
