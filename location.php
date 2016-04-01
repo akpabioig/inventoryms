e5380a9dab4a3a3bceec932e1bf2810e485b82cd
Ajibola <jibolso@gmail.com>
2016-04-01 22:04:25 +0100

    Populating dynamic content for Location

--- location.php
+++ location.php
@@ -100,7 +100,7 @@ include('connection.php');
         <h2>WareHouse / Yards </h2>
         <p>Select The Location within the Warehouse or Yard you would like to explore its' contents</p>
         <div class = "locbutt">
-            <input type="button" id = "a" onclick="divClicked = 'a';">
+            <input type="button" id = "a" onclick="getid('a');">
                 <img src= "location/a1.jpeg" style{height="150" width="150"} class ="locicons"/>
             </a>
             <a id="a2">
@@ -216,6 +216,12 @@ include('connection.php');
 
     // Get the modal
     var divClicked = "";
+
+    function getid(n){
+        if(n === 'a'){
+            divClicked = 'a';
+        }
+    }
     var modal = document.getElementById('myModal');
 
     // Get the button that opens the modal
