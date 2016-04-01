1e377823ca6c15269f6ed1557f122473558e8b1b
Ajibola <jibolso@gmail.com>
2016-04-01 21:05:43 +0100

    Generating dynamic content based on the location clicked

--- location.php
+++ location.php
@@ -100,7 +100,7 @@ include('connection.php');
         <h2>WareHouse / Yards </h2>
         <p>Select The Location within the Warehouse or Yard you would like to explore its' contents</p>
         <div class = "locbutt">
-            <a id = "a1" onclick=" var divClicked = 'a1'">
+            <a id = "a1" onclick="document.getElementById(a1);">
                 <img src= "location/a1.jpeg" style{height="150" width="150"} class ="locicons"/>
             </a>
             <a id="myBtn">
