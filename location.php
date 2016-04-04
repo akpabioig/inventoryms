4ddbb44794ac75c6a7bd954849b44543a0062550
akpabioignatius <1414987@rgu.ac.uk>
2016-04-04 15:11:10 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -183,8 +183,7 @@ include('connection.php');
 
                         $sql= "SELECT productid, productserialnumber, productname, productdescription, initialstockprice, wholesaleprice, retailprice
                                 FROM addproduct
-                                WHERE locationid =
-                                {<script type='application/javascript'> document.write(a1) </script>}";
+                                WHERE locationid = {print('WH Loc A1')}";
                         $result = mysqli_query($db, $sql);
                         if(mysqli_num_rows($result) == 1 || mysqli_num_rows($result) >1){
 
