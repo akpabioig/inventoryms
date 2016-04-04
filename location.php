9eb73d7e1d2e7cc303ef13e7fca50afd8bf0f547
akpabioignatius <1414987@rgu.ac.uk>
2016-04-04 14:27:37 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -214,38 +214,37 @@ include('connection.php');
     </section>
 </div>
 <script>
-    var divClicked;
     function pass(n){
         if(n === 'a'){
-            divClicked = "a";
+            // Get the modal
+            var divClicked = "a";
+            var modal = document.getElementById('myModal');
+
+            // Get the button that opens the modal
+            var btn = document.getElementById(divClicked);
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
         }
     }
-    // Get the modal
-    //var divClicked = "a";
-    var modal = document.getElementById('myModal');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById(divClicked);
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
 </script>
 <footer>
     <p>&copy; Akpabio Ignatius, 2016</p>
