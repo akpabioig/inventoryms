14833ce5ef0d4ba8aad24d2f46744c49753f7c6b
akpabioignatius <1414987@rgu.ac.uk>
2016-04-04 14:14:12 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -217,35 +217,34 @@ include('connection.php');
 
     //var divClicked;
     $("#a").on('click', function() {
-         divClicked = 'a';
-        //alert('clicked');
-    });
+        // Get the modal
+        var modal = document.getElementById('myModal');
 
-    // Get the modal
-    var modal = document.getElementById('myModal');
+        // Get the button that opens the modal
+        var btn = document.getElementById('a');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById(divClicked);
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+        // When the user clicks the button, open the modal
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
 
-    // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
             modal.style.display = "none";
         }
-    }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    });
+
+
 </script>
 <footer>
     <p>&copy; Akpabio Ignatius, 2016</p>
