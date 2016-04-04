4644459ffe03378e90a13a9418d1e334688246bc
akpabioignatius <1414987@rgu.ac.uk>
2016-04-04 14:40:26 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -103,7 +103,7 @@ include('connection.php');
             <a id="a" onclick="return pass('a');">
                 <img src= "location/a1.jpeg" style{height="150" width="150"} class ="locicons"/>
             </a>
-            <a id="a2">
+            <a id="b">
                 <img src= "location/a2.jpg" style{height="150" width="150"} class ="locicons"/>
             </a>
             <a id="a3" href= "#">
@@ -226,6 +226,32 @@ include('connection.php');
 
             // When the user clicks the button, open the modal
             a.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        }else if(n === 'b'){
+            // Get the modal
+            var modal = document.getElementById('myModal');
+
+            // Get the button that opens the modal
+            //var btn = document.getElementById('a');
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal
+            b.onclick = function() {
                 modal.style.display = "block";
             }
 
