26083acddc0027d2c3a3b6a235b3c33ef6770c33
akpabioignatius <1414987@rgu.ac.uk>
2016-04-04 14:00:23 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -100,10 +100,10 @@ include('connection.php');
         <h2>WareHouse / Yards </h2>
         <p>Select The Location within the Warehouse or Yard you would like to explore its' contents</p>
         <div class = "locbutt">
-            <a id="a" onclick="return pass('a');">
+            <a id="a" onclick="return pass('id');">
                 <img src= "location/a1.jpeg" style{height="150" width="150"} class ="locicons"/>
             </a>
-            <a id="a2" onclick="return pass('b');">
+            <a id="a2">
                 <img src= "location/a2.jpg" style{height="150" width="150"} class ="locicons"/>
             </a>
             <a id="a3" href= "#">
