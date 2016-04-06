a193965e80d150975bfa1ff3db6a1dcea8f0972e
akpabioignatius <1414987@rgu.ac.uk>
2016-04-06 19:41:26 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -96,6 +96,7 @@ include('connection.php');
                     // When the user clicks the button, open the modal
                     a.onclick = function() {
                         modal.style.display = "block";
+                        <?php $a = "WH Location A1"; ?>
 
 
                     }
@@ -127,6 +128,7 @@ include('connection.php');
                     // When the user clicks the button, open the modal
                     b.onclick = function() {
                         modal.style.display = "block";
+                        <?php $b = "WH Location B4"; ?>
                     }
 
                     // When the user clicks on <span> (x), close the modal
@@ -201,17 +203,6 @@ include('connection.php');
                     }
                 }
             }*/
-        }
-
-    </script>
-    <script type="application/javascript">
-        function use(){
-            $.ajax({
-                type: "POST",
-                url: "passlocation.php",
-                data: {a1: "WH Location A1"},
-                success: console.log("done")
-            })
         }
 
     </script>
@@ -318,44 +309,8 @@ include('connection.php');
                         <th>Wholesale Price</th>
                         <th>Retail Price</th>
                     </tr>
-                        <?php
-
-                        /*
-                         * $sql= "SELECT productid, productserialnumber, productname, productdescription, initialstockprice, wholesaleprice, retailprice
-                                FROM addproduct
-                                WHERE locationid = 'WH Location A1'";
-                         *
-                         *
-                         */
-                        require("passlocation.php");
-                        echo "Location:" . ' '. $a1;
-                        /*
-                        $sql= "SELECT productid, productserialnumber, productname, productdescription, initialstockprice, wholesaleprice, retailprice
-                                FROM addproduct
-                                WHERE locationid = '{$loc}'"; //get Js
-                        $result = mysqli_query($db, $sql);
-                        if(mysqli_num_rows($result) == 1 || mysqli_num_rows($result) >1){
-
-                            while($row = $result -> fetch_array()){
-                                echo "
-
-                                <tr>
-                        <td><input type=\"text\" id = \"productid\" name= \"productid\"  value = \"{$row['productid']}\" class = \"tablefield\"></td>
-                        <td><input type=\"text\" id = \"productsn\" name= \"productsn\"  value = \"{$row['productserialnumber']}\" class = \"tablefield\"></td>
-                        <td><input type=\"text\" id = \"productname\" name= \"productname\"  value = \"{$row['productname']}\" class = \"tablefield\"></td>
-                        <td><input type=\"text\" id = \"productdesc\" name= \"productdesc\"  value = \"{$row['productdescription']}\" class = \"tablefield\"></td>
-                        <td><input type=\"text\" id = \"isp\" name= \"isp\"  value = \"{$row['initialstockprice']}\" class = \"tablefield\"></td>
-                        <td><input type=\"text\" id = \"wp\" name= \"wp\"  value = \"{$row['wholesaleprice']}\" class = \"tablefield\"></td>
-                        <td><input type=\"text\" id = \"rp\" name= \"rp\"  value = \"{$row['retailprice']}\" class = \"tablefield\"></td>
-                    </tr>
-
-                                ";
-                            }
-                        }else{
-                            echo '<option> No Result Found </option>';
-                        }
-                        */
-                        ?>
+                       <?php
+                       include('passlocation.php'); ?>
 
                     </table>
                 </div>
--- passlocation.php
+++ passlocation.php
@@ -1,4 +1,66 @@
 <?php
-//$a1 = "khb";
-$a1 = $_POST['a1'];
+
+                        /*
+                         * $sql= "SELECT productid, productserialnumber, productname, productdescription, initialstockprice, wholesaleprice, retailprice
+                                FROM addproduct
+                                WHERE locationid = 'WH Location A1'";
+                         *
+                         *
+                         */
+                        //checking the variable
+                        if(isset($a)){
+                            $sql= "SELECT productid, productserialnumber, productname, productdescription, initialstockprice, wholesaleprice, retailprice
+                                FROM addproduct
+                                WHERE locationid = '{$loc}'"; //get Js
+                            $result = mysqli_query($db, $sql);
+                            if(mysqli_num_rows($result) == 1 || mysqli_num_rows($result) >1){
+
+                                while($row = $result -> fetch_array()){
+                                    echo "
+
+                                <tr>
+                        <td><input type=\"text\" id = \"productid\" name= \"productid\"  value = \"{$row['productid']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"productsn\" name= \"productsn\"  value = \"{$row['productserialnumber']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"productname\" name= \"productname\"  value = \"{$row['productname']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"productdesc\" name= \"productdesc\"  value = \"{$row['productdescription']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"isp\" name= \"isp\"  value = \"{$row['initialstockprice']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"wp\" name= \"wp\"  value = \"{$row['wholesaleprice']}\" class = \"tablefield\"></td>
+                        <td><input type=\"text\" id = \"rp\" name= \"rp\"  value = \"{$row['retailprice']}\" class = \"tablefield\"></td>
+                    </tr>
+
+                                ";
+                                }
+                            }else{
+                                echo '<option> No Result Found </option>';
+                            }
+                        }else if(isset($b)){
+                            $sql= "SELECT productid, productserialnumber, productname, productdescription, initialstockprice, wholesaleprice, retailprice
+                                FROM addproduct
+                                WHERE locationid = '{$b}'"; //get Js
+                            $result = mysqli_query($db, $sql);
+                            if(mysqli_num_rows($result) == 1 || mysqli_num_rows($result) >1){
+
+                                while($row = $result -> fetch_array()){
+                                    echo "
+
+                                <tr>
+                                <td><input type=\"text\" id = \"productid\" name= \"productid\"  value = \"{$row['productid']}\" class = \"tablefield\"></td>
+                                <td><input type=\"text\" id = \"productsn\" name= \"productsn\"  value = \"{$row['productserialnumber']}\" class = \"tablefield\"></td>
+                                <td><input type=\"text\" id = \"productname\" name= \"productname\"  value = \"{$row['productname']}\" class = \"tablefield\"></td>
+                                <td><input type=\"text\" id = \"productdesc\" name= \"productdesc\"  value = \"{$row['productdescription']}\" class = \"tablefield\"></td>
+                                <td><input type=\"text\" id = \"isp\" name= \"isp\"  value = \"{$row['initialstockprice']}\" class = \"tablefield\"></td>
+                                <td><input type=\"text\" id = \"wp\" name= \"wp\"  value = \"{$row['wholesaleprice']}\" class = \"tablefield\"></td>
+                                <td><input type=\"text\" id = \"rp\" name= \"rp\"  value = \"{$row['retailprice']}\" class = \"tablefield\"></td>
+                                </tr>
+
+                                ";
+                                }
+                            }else{
+                                echo '<option> No Result Found </option>';
+                            }
+                        }
+
+                        ?>
+
+
 ?>
