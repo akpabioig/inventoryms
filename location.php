5016c8384fea2a0433f8f94efd23ff883f0bb9b7
akpabioignatius <1414987@rgu.ac.uk>
2016-04-06 21:13:41 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -42,9 +42,10 @@ include('connection.php');
 
         /* The Close Button */
         .close {
-        color: #aaaaaa;
-        float: right;
-        font-size: 28px;  font-weight: bold;
+    color: #aaaaaa;
+    float: right;
+    font-size: 28px;
+            font-weight: bold;
         }
 
         .close:hover,
@@ -270,9 +271,8 @@ include('connection.php');
         }
     }
 
-
-
      // WH LOCATION A2
+
     var modal1 = document.getElementById('myModal1');
 
     // Get the button that opens the modal
@@ -287,8 +287,8 @@ include('connection.php');
     }
 
     // When the user clicks on <span> (x), close the modal
-    span1.onclick = function() {
-        this.style.display = "none";
+    span.onclick = function() {
+        modal1.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
