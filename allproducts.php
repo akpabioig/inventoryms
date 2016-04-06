6cccf3629c18a0382986c02aeb2bff5ae9b34bf2
akpabioignatius <1414987@rgu.ac.uk>
2016-04-07 00:56:32 +0100

    Getting  alert on delete to work

--- allproducts.php
+++ allproducts.php
@@ -72,8 +72,8 @@
                         <td><input type=\"text\" id = \"isp\" name= \"isp\"  value = \"{$row['initialstockprice']}\" class = \"tablefield\" disabled></td>
                         <td><input type=\"text\" id = \"wp\" name= \"wp\"  value = \"{$row['wholesaleprice']}\" class = \"tablefield\" disabled></td>
                         <td><input type=\"text\" id = \"rp\" name= \"rp\"  value = \"{$row['retailprice']}\" class = \"tablefield\" disabled></td>
-                        <td><a href='edit-product.php?prodid={$row['productid']}'>EDIT  </a></td>
-                        <td><a href='deleteproduct.php?prodid={$row['productid']}''>DELETE</a> </td>
+                        <td><a href='edit-product.php?prodid={$row['productid']}'><img src = 'images/edit.png' style{height=\"50\" width=\"50\"}></a></td>
+                        <td><a href='deleteproduct.php?prodid={$row['productid']}'> <img src = 'images/delete.png' style{height=\"50\" width=\"50\"}></a> </td>
                     </tr>
 
                                 ";
