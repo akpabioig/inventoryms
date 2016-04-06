0a149f16561f80d1b860c3bef7f40d43ae7cc81b
akpabioignatius <1414987@rgu.ac.uk>
2016-04-06 21:25:55 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -294,7 +294,7 @@ include('connection.php');
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal1) {
-            modal.style.display = "none";
+            modal1.style.display = "none";
         }
     }
 </script>
