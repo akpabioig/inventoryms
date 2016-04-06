2d00618a7735cab93d431733ad696198353d57f5
akpabioignatius <1414987@rgu.ac.uk>
2016-04-06 18:40:58 +0100

    Making the location dynamic field work

--- location.php
+++ location.php
@@ -211,7 +211,7 @@ include('connection.php');
                 type: "POST",
                 url: "passlocation.php",
                 data: { a1: "WH Location A1"},
-                success: console.log("done");
+                success: console.log("done")
             });
         })
     </script>
