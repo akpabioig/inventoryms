52d6061adb10f420352d80e20873e6598a5b0b3a
akpabioignatius <1414987@rgu.ac.uk>
2016-04-06 22:16:56 +0100

    Getting the edit product to work

--- allproducts.php
+++ allproducts.php
@@ -72,7 +72,7 @@
                         <td><input type=\"text\" id = \"isp\" name= \"isp\"  value = \"{$row['initialstockprice']}\" class = \"tablefield\" disabled></td>
                         <td><input type=\"text\" id = \"wp\" name= \"wp\"  value = \"{$row['wholesaleprice']}\" class = \"tablefield\" disabled></td>
                         <td><input type=\"text\" id = \"rp\" name= \"rp\"  value = \"{$row['retailprice']}\" class = \"tablefield\" disabled></td>
-                        <td><a href='edit-product.php?edit=' {$row['productid']}'>Edit</a> </td> &nbsp;
+                        <td><a href='edit-product.php?edit={$row['productid']}>Edit</a> </td> &nbsp;
                         <td><a href=''>Delete</a> </td>
                     </tr>
 
