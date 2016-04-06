1e3ae1545a66026243e8c68484c790761ce5d462
akpabioignatius <1414987@rgu.ac.uk>
2016-04-07 00:02:50 +0100

    Getting  alert on delete to work

--- deleteproduct.php
+++ deleteproduct.php
@@ -1,9 +1,11 @@
-<!--<script type="application/javascript">
+<script type="application/javascript">
 if(confirm('Are you sure you want to delete this record')){
-    <?php //$continue = TRUE; ?>
+    <?php $continue = TRUE; ?>
+}else {
+    <?php $continue = FALSE; ?>
+    window.location="allproducts.php";
 }
 </script>
--->
 <?php
 if($continue){
     $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3',array(PDO::ATTR_EMULATE_PREPARES => false,
