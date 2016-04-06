34712de170e54ad26359489fa2bb8b67bf335a8d
akpabioignatius <1414987@rgu.ac.uk>
2016-04-06 23:38:11 +0100

    Getting the edit product to work

--- deleteproduct.php
+++ deleteproduct.php
@@ -1,22 +1,25 @@
-<script type="application/javascript">
-    window.onload = function(e) {
-        return confirm('Are you sure you want to delete this record?');
-    };
-</script>
+
 <?php
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3',array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
     if(isset($_GET['prodid'])){
     $prodId = $_GET['prodid'];
+        echo "<script>";
+        echo "if(!confirm('Are you sure you want to delete this record')){";
+        echo "window.location = 'allproducts.php'";
+        echo "}";
+        echo "</script>";
         try {
             $sql = "DELETE FROM addproduct
                     WHERE productid = {$prodId}";
             $sth = $db->query($sql);
+
         }catch(PDOException $e){
             echo $e->getMessage();
         }
-        header("Location: allproducts.php");
+
+
     }else{
         header("Location: allproducts.php");
     }
