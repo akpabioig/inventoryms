e1f44d6b30c17d901b87c1b710ea42fbad84ea6e
akpabioignatius <1414987@rgu.ac.uk>
2016-04-06 23:30:39 +0100

    Getting the edit product to work

--- deleteproduct.php
+++ deleteproduct.php
@@ -1,7 +1,7 @@
 <script type="application/javascript">
-    window.onload{
-        confirm("Are you sure you want to delete this record?");
-    }
+    window.onload = function(e) {
+        return confirm('Are you sure you want to delete this record?');
+    };
 </script>
 <?php
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3',array(PDO::ATTR_EMULATE_PREPARES => false,
