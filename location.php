4ec87f5c82eb6981f84f529295d08296b92876d8
akpabioignatius <1414987@rgu.ac.uk>
2016-04-08 16:35:19 +0100

    Getting  alert on delete to work

--- location.php
+++ location.php
@@ -285,14 +285,14 @@ include('connection.php');
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+    /*span.onclick = function() {
         modal1.style.display = "none";
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal1) {
-            modal1.style.display = "none";
+            modal1.style.display = "none";*/
         }
     }
 </script>
