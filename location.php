b541a426092378540284208f9a618854f9f50938
akpabioignatius <1414987@rgu.ac.uk>
2016-04-08 17:22:52 +0100

    Getting  alert on delete to work

--- location.php
+++ location.php
@@ -243,7 +243,7 @@ include('connection.php');
 
             <div id="myModal2" class="modal">
                 <div class="modal-content">
-                    <span class="close1">×</span>
+                    <span class="close1">x</span>
                     <header>
                         <h1> Location : WareHouse Location A3</h1>
                     </header>
