3357c1e655840209d53a5c6c5928697c939f03e2
akpabioignatius <1414987@rgu.ac.uk>
2016-04-14 20:57:55 +0100

    NewSales Page, getting the dynmic select and fields population to work

--- newsales.php
+++ newsales.php
@@ -121,6 +121,7 @@
                                         echo '<script type="application/javascript">';
                                         echo 'var price[] = ' . json_encode($row['retailprice']) . ';';
                                         echo 'var locid[] = ' . json_encode($row['locationid']) . ';';
+                                        echo 'alert(price[1]);';
                                         echo '</script>';
                                     }
                                 }else{
