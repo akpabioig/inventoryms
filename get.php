05ccf67b50093ccdbaf1d979da225a3cc12430c1
akpabioignatius <1414987@rgu.ac.uk>
2016-04-14 21:50:29 +0100

    NewSales Page, getting the dynmic select and fields population to work

--- get.php
+++ get.php
@@ -9,7 +9,7 @@
 include('connection.php');
 
 $rq = $_POST['myvariant'];
-$sql = "SELECT * FROM addproduct WHERE productname ='{$rq}'";
+$sql = "SELECT * FROM addproduct WHERE productname ='$rq'";
 $result = mysqli_query($db, $sql);
 echo $result['productid'];
 ?>
