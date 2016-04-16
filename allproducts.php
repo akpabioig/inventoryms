9553554727e4b8590f571fa8bd33c216650337ab
akpabioignatius <1414987@rgu.ac.uk>
2016-04-16 17:21:47 +0100

    Export to Excel

--- allproducts.php
+++ allproducts.php
@@ -57,7 +57,6 @@ $result = mysqli_query($db, $sql);
                 <option value="locationid"> PRODUCT LOCATION</option>
                 <option value="suppliername"> SUPPLIER NAME</option>
             </select>
-            <button href="export.php">EXPORT</button>
                 <table>
                 <tr>
                     <th> ID </th>
