16be2bebe4cd082c38d5368e197ab7f3bdb6b87d
akpabioignatius <1414987@rgu.ac.uk>
2016-04-16 15:42:43 +0100

    Checking logged in users

--- allproducts.php
+++ allproducts.php
@@ -59,7 +59,7 @@ $result = mysqli_query($db, $sql);
                 <option value="locationid"> PRODUCT LOCATION</option>
                 <option value="suppliername"> SUPPLIER NAME</option>
             </select>
-            <button onClick="$('#producttable').tableExport({type:'excel',escape:'false'});">Export To Excel</button>
+            <button onclick="tableToExcel('producttable', 'Thtuyhij')">Export To Excel</button>
             <table id="producttable">
                 <tr>
                     <th> ID </th>
@@ -119,7 +119,25 @@ $result = mysqli_query($db, $sql);
             });
         });
     </script>
-
+    <script>
+        var tableToExcel = (function () {
+            var uri = 'data:application/vnd.ms-excel;base64,'
+                , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+                , base64 = function (s) {
+                return window.btoa(unescape(encodeURIComponent(s)))
+            }
+                , format = function (s, c) {
+                return s.replace(/{(\w+)}/g, function (m, p) {
+                    return c[p];
+                })
+            }
+            return function (table, name) {
+                if (!table.nodeType) table = document.getElementById(table)
+                var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+                window.location.href = uri + base64(format(template, ctx))
+            }
+        })()
+    </script>
 </div>
 <footer>
     <p>&copy; Akpabio Ignatius, 2016</p>
