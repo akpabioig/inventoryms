ef8db8d90182c7761ad9871f681b12dcae1441c1
akpabioignatius <1414987@rgu.ac.uk>
2016-04-16 15:45:28 +0100

    Checking logged in users

--- allproducts.php
+++ allproducts.php
@@ -25,8 +25,6 @@ $result = mysqli_query($db, $sql);
     <script src="scripting.js"></script>
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="tableExport.js"></script>
-    <script type="text/javascript" src="jquery.base64.js"></script>
 </head>
 <body>
 <nav class="w3-sidenav w3-black" style="width:102px">
@@ -59,7 +57,6 @@ $result = mysqli_query($db, $sql);
                 <option value="locationid"> PRODUCT LOCATION</option>
                 <option value="suppliername"> SUPPLIER NAME</option>
             </select>
-            <button onclick="tableToExcel('producttable', 'Thtuyhij')">Export To Excel</button>
             <table id="producttable">
                 <tr>
                     <th> ID </th>
@@ -118,25 +115,6 @@ $result = mysqli_query($db, $sql);
                     });
             });
         });
-    </script>
-    <script>
-        var tableToExcel = (function () {
-            var uri = 'data:application/vnd.ms-excel;base64,'
-                , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
-                , base64 = function (s) {
-                return window.btoa(unescape(encodeURIComponent(s)))
-            }
-                , format = function (s, c) {
-                return s.replace(/{(\w+)}/g, function (m, p) {
-                    return c[p];
-                })
-            }
-            return function (table, name) {
-                if (!table.nodeType) table = document.getElementById(table)
-                var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-                window.location.href = uri + base64(format(template, ctx))
-            }
-        })()
     </script>
 </div>
 <footer>
