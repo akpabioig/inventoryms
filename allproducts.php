cab77628435420e1a15acb3d406e36bf1da7ad8f
akpabioignatius <1414987@rgu.ac.uk>
2016-04-16 14:56:06 +0100

    Checking logged in users

--- allproducts.php
+++ allproducts.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
+/*session_start();
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
-}
+}*/
 
 include('connection.php');
 $sql = "SELECT addproduct.productid, addproduct.productserialnumber, addproduct.productname, addproduct.productdescription,
