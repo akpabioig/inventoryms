fdffb7a8dae2361d84c361d859f7cd926b3b89ed
akpabioignatius <1414987@rgu.ac.uk>
2016-04-16 22:51:26 +0100

    Export to Excel

--- allproducts.php
+++ allproducts.php
@@ -60,7 +60,8 @@ $result = mysqli_query($db, $sql);
                 <option value="locationid"> PRODUCT LOCATION</option>
                 <option value="suppliername"> SUPPLIER NAME</option>
             </select>
-            <div id="producttable">
+            <a id="export"> Export</a>
+            <nav id="producttable">
                 <table>
                 <tr>
                     <th> ID </th>
@@ -78,7 +79,7 @@ $result = mysqli_query($db, $sql);
                 if(mysqli_num_rows($result) == 1 || mysqli_num_rows($result) >1){
                     while($row = $result -> fetch_array()){
                         echo "
-                                <tr>
+                    <tr>
                         <td><input type=\"text\" id = \"productid\" name= \"productid\"  value = \"{$row['productid']}\" class = \"tablefield\" disabled></td>
                         <td><input type=\"text\" id = \"productsn\" name= \"productsn\"  value = \"{$row['productserialnumber']}\" class = \"tablefield\" disabled></td>
                         <td><input type=\"text\" id = \"productname\" name= \"productname\"  value = \"{$row['productname']}\" class = \"tablefield\" disabled></td>
@@ -98,7 +99,7 @@ $result = mysqli_query($db, $sql);
                 ?>
                 </tbody>
             </table>
-            </div>
+            </nav>
         </div>
         </form>
     </section>
@@ -119,7 +120,7 @@ $result = mysqli_query($db, $sql);
         });
     </script>
     <script>
-        /*$(document).ready(function () {
+        $(document).ready(function () {
             $("#export").click(function (e) {
                 //getting values of current time for generating the file name
                 var dt = new Date();
@@ -143,7 +144,7 @@ $result = mysqli_query($db, $sql);
                 //just in case, prevent default behaviour
                 e.preventDefault();
             });
-         });*/
+        });
     </script>
 </div>
 <footer>
