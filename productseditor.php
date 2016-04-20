8cd993382be969a8ed8b8dfa5b8f80bb90e48919
akpabioignatius <1414987@rgu.ac.uk>
2016-04-20 23:57:52 +0100

    Order Fulfilment

--- productseditor.php
+++ productseditor.php
@@ -87,7 +87,7 @@ $result = mysqli_query($db, $sql);
                         <td><input type=\"text\" id = \"wp\" name= \"wp\"  value = \"{$row['wholesaleprice']}\" class = \"tablefield\" disabled></td>
                         <td><input type=\"text\" id = \"rp\" name= \"rp\"  value = \"{$row['retailprice']}\" class = \"tablefield\" disabled></td>
                         <td><a href='edit-product.php?prodid={$row['productid']}'><img src = 'images/edit.png' style{height=\"25\" width=\"25\"}></a></td>
-                        <td><a href='deleteproduct.php?prodid={$row['productid']}'> <img src = 'images/delete.png' style{height=\"25\" width=\"25\"}></a> </td>
+                        <td><a href='deleteproduct.php?prodid={$row['productid']}' onclick='return deleleconfig()'> <img src = 'images/delete.png' style{height=\"25\" width=\"25\"}></a> </td>
                     </tr>
 
                                 ";
@@ -116,6 +116,17 @@ $result = mysqli_query($db, $sql);
                     });
             });
         });
+    </script>
+
+    <script>
+        function deleletconfig() {
+
+            var del = confirm("Are you sure you want to delete this record?");
+            if (del == true) {
+                alert("record deleted")
+            }
+            return del;
+        }
     </script>
 </div>
 <footer>
