7b62977c8067e84af0992f9117aa2f6370eb5d2a
akpabioignatius <1414987@rgu.ac.uk>
2016-04-21 21:50:55 +0100

    Order Fulfilment

--- adminhomepage.php
+++ adminhomepage.php
@@ -3,7 +3,6 @@ session_start();
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
 }
-
 include('connection.php');
 ?>
 
--- adminlogin.php
+++ adminlogin.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-
 </head>
 <body>
 <div style="margin-left:70px">
