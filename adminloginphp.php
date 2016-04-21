a625d054f6e7be2c7ba10ae0f92fa6b642ebad9d
akpabioignatius <1414987@rgu.ac.uk>
2016-04-21 21:35:26 +0100

    Order Fulfilment

--- adminloginphp.php
+++ adminloginphp.php
@@ -4,13 +4,11 @@ session_start();
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-$adminusername = 'admin';
-$adminpassword = 'admin';
-/*$adminusername = htmlspecialchars($_POST['adminusername']);
-$adminpassword = htmlspecialchars($_POST['adminpassword']);*/
+$adminusername = htmlspecialchars($_POST['adminusername']);
+$adminpassword = htmlspecialchars($_POST['adminpassword']);
 
 try {
-    $sql = "SELECT * FROM login
+    $sql = "SELECT * FROM adminlogin
                 WHERE username = '$adminusername'
                 AND password = '$adminpassword' LIMIT 1";
     $sth = $db->query($sql);
