108cdb8bc03a7bdcdc11fc1ac27393bd4c6b3e11
akpabioignatius <1414987@rgu.ac.uk>
2016-04-22 14:47:43 +0100

    Admin Login Config

--- updateuser.php
+++ updateuser.php
@@ -3,31 +3,22 @@
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-echo $title = htmlspecialchars($_POST['title']);
-echo "<br>";
-echo $firstname = htmlspecialchars($_POST['firstname']);
-echo "<br>";
-echo $middlename = htmlspecialchars($_POST['middlename']);
-echo "<br>";
-echo $lastname = htmlspecialchars($_POST['lastname']);
-echo "<br>";
-echo $position = htmlspecialchars($_POST['position']);
-echo "<br>";
-
-echo $username = htmlspecialchars($_POST['username']);
-echo "<br>";
-echo $password = htmlspecialchars($_POST['password']);
-echo "<br>";
-echo $uid = htmlspecialchars($_POST['uid']);
-echo "<br>";
-/*try {
+$title = htmlspecialchars($_POST['title']);
+$firstname = htmlspecialchars($_POST['firstname']);
+$middlename = htmlspecialchars($_POST['middlename']);
+$lastname = htmlspecialchars($_POST['lastname']);
+$position = htmlspecialchars($_POST['position']);
+$username = htmlspecialchars($_POST['username']);
+$password = htmlspecialchars($_POST['password']);
+$uid = htmlspecialchars($_POST['uid']);
+try {
     $sql = "UPDATE user
                 SET title = '{$title}',
                     firstname = '{$firstname}',
                     middlename = '{$middlename}',
                     lastname = '{$lastname}',
                     staffposition = '{$staffposition}',
-                    WHERE userid = {$userId}";
+                    WHERE userid = {$uid}";
     $sth = $db->query($sql);
 } catch (PDOException $e) {
     echo $e->getMessage();
@@ -37,10 +28,10 @@ try {
     $sql = "UPDATE login
                 SET username = '{$username}',
                     password = '{$password}',
-                    WHERE userid = {$userId}";
+                    WHERE userid = {$uid}";
     $sth = $db->query($sql);
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
-header("Location: edit-user.php");*/
+header("Location: edit-user.php");
 ?>
