3646d3704d66108ba620f1b55664714f3242de68
akpabioignatius <1414987@rgu.ac.uk>
2016-04-25 14:56:47 +0100

    update stock level

--- stocklevel.php
+++ stocklevel.php
@@ -9,7 +9,7 @@ $sql = "SELECT stocklevel.stocklevelid, addproduct.productname, stocklevel.stock
                           FROM stocklevel, addproduct
                           WHERE stocklevel.productid = addproduct.productid
                           ORDER BY productname";
-"UPDATE stocklevel
+$sql = "UPDATE stocklevel
                 SET stocklevel.level = 'No Restock !!!'
                     WHERE stocklevel.stockbalance > 19";
 $result = mysqli_query($db, $sql);
