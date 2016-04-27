ebc428b8c05984c49e498a5704db0a5650de16c6
akpabioignatius <1414987@rgu.ac.uk>
2016-04-27 14:16:25 +0100

    update stock level

--- updatefulfilorder.php
+++ updatefulfilorder.php
@@ -2,6 +2,7 @@
 include('connection.php');
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$text = "Cannot fulfill Order because Quantity in Stock is less that Quantity Ordered";
 if (isset($_GET['salesid'])) {
     $soId = $_GET['salesid'];
     $sqlselect = "SELECT * FROM salesorder WHERE sid = $soId";
@@ -20,7 +21,10 @@ if (isset($_GET['salesid'])) {
     $stock2 = mysqli_query($db, $quantity);
 
     if ($stock2 > $stock1) {
-        echo "<script type='text/javascript'>alert('Cannot fulfill Order because Quantity in Stock is less that Quantity Ordered');</script>";
+        function echotext($text)
+        {
+            echo "<script type='text/javascript'>alert('{$text}');</script>";
+        }
         return;
     } elseif ($stock2 < $stock1) {
         try {
