781a223eec1a49f81d03bf79f92ca68fc6daf5f2
akpabioignatius <1414987@rgu.ac.uk>
2016-04-27 20:18:33 +0100

    update stock level

--- updatefulfilorder.php
+++ updatefulfilorder.php
@@ -1,9 +1,9 @@
 <?php
 include('connection.php');
-$sql = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
+$db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-$f = $sql->query("SELECT stocklevel.stockbalance
+$f = $db->query("SELECT stocklevel.stockbalance
                 FROM stocklevel, salesitem, salesorder
                 WHERE stocklevel.productid = salesitem.productid
                 AND salesitem.sid = salesorder.sid
