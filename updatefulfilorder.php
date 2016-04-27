a4fe530b523fa2f35cd3722b2042de6798e11d86
akpabioignatius <1414987@rgu.ac.uk>
2016-04-27 04:17:11 +0100

    update stock level

--- updatefulfilorder.php
+++ updatefulfilorder.php
@@ -2,32 +2,45 @@
 include('connection.php');
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
 if (isset($_GET['salesid'])) {
     $soId = $_GET['salesid'];
     $sqlselect = "SELECT * FROM salesorder WHERE sid = $soId";
     $getResult = mysqli_query($db, $sqlselect);
 
-    try {
-        $sql = "UPDATE salesorder
+    $stock1 = "SELECT stocklevel.stockbalance
+                FROM stocklevel, salesitem, salesorder
+                WHERE stocklevel.productid = salesitem.productid
+                AND salesitem.sid = salesorder.sid
+                AND salesorder.sid = {$soId}";
+
+    $stock2 = "SELECT quantity
+               FROM salesitem
+               WHERE sid = {$soId}";
+
+    if ($stock2 > $stock1) {
+        echo "Cannot Fulfill Order Because Stock Quantity Too Low";
+        return false;
+    } elseif ($stock2 < $stock1) {
+        try {
+            $sql = "UPDATE salesorder
                 SET status = 'fulfilled'
                     WHERE sid = {$soId}";
-        $sth = $db->query($sql);
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-    }
-    try {
-        $sql1 = "UPDATE stocklevel, salesorder, salesitem
+            $sth = $db->query($sql);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        try {
+            $sql1 = "UPDATE stocklevel, salesorder, salesitem
                 SET stocklevel.stockbalance = stocklevel.stockbalance - salesitem.quantity
                 WHERE stocklevel.productid = salesitem.productid
                 AND salesitem.sid = salesorder.sid
                 AND salesorder.status = 'fulfilled'
                 AND salesorder.sid = {$soId}";
-        $sth1 = $db->query($sql1);
-    } catch (PDOException $f) {
-        echo $f->getMessage();
+            $sth1 = $db->query($sql1);
+        } catch (PDOException $f) {
+            echo $f->getMessage();
+        }
     }
-
 }
 
     if (isset($_GET['purid'])) {
