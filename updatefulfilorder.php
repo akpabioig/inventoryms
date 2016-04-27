b62d7c3425e450024de76af6a070bad578090bef
akpabioignatius <1414987@rgu.ac.uk>
2016-04-27 16:24:21 +0100

    update stock level

--- updatefulfilorder.php
+++ updatefulfilorder.php
@@ -2,28 +2,28 @@
 //include('connection.php');
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-if (isset($_GET['salesid'])) {
-    $soId = $_GET['salesid'];
-    $sqlselect = "SELECT * FROM salesorder WHERE sid = $soId";
-    $getResult = mysqli_query($db, $sqlselect);
 
-    //pdo codes
-    $stock1 = $sql6->query("SELECT stocklevel.stockbalance 
+//pdo codes
+$stock1 = $sql6->query("SELECT stocklevel.stockbalance 
                     FROM stocklevel, salesitem, salesorder 
                     WHERE stocklevel.productid = salesitem.productid 
                     AND salesitem.sid = salesorder.sid 
                     AND salesorder.sid = {$soId}");
-    $stock1->setFetchMode(PDO::FETCH_ASSOC);
-     
-    $stock1->fetchAll()[0]['stockbalance'];
+$stock1->setFetchMode(PDO::FETCH_ASSOC);
+$stock1->fetchAll()[0]['stockbalance'];
 
 
-    $stock2 = $sql7->query("SELECT quantity
+$stock2 = $sql7->query("SELECT quantity
                FROM salesitem
                WHERE sid = {$soId}");
-    $stock2->setFetchMode(PDO::FETCH_ASSOC);
-    $stock2->fetchAll()[0]['quantity'];
-    //end of pdo codes
+$stock2->setFetchMode(PDO::FETCH_ASSOC);
+$stock2->fetchAll()[0]['quantity'];
+//end of pdo codes
+
+if (isset($_GET['salesid'])) {
+    $soId = $_GET['salesid'];
+    $sqlselect = "SELECT * FROM salesorder WHERE sid = $soId";
+    $getResult = mysqli_query($db, $sqlselect);
 
     if ($stock2 > $stock1) {
         echo "<script type='text/javascript'>
