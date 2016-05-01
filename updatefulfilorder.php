a9a9f2691e59cb5bfb7d0a8302fdc9672aa067c6
akpabioignatius <1414987@rgu.ac.uk>
2016-05-01 23:31:42 +0100

    update stock level

--- updatefulfilorder.php
+++ updatefulfilorder.php
@@ -2,8 +2,7 @@
 include('connection.php');
 
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_STRINGIFY_FETCHES => false, PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL));
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 if (isset($_GET['salesid'])) {
     $soId = $_GET['salesid'];
