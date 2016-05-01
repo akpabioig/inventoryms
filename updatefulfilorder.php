84e27ca4a2d53b84fd40526035d37f9901ff961c
akpabioignatius <1414987@rgu.ac.uk>
2016-05-02 00:07:13 +0100

    update stock level

--- updatefulfilorder.php
+++ updatefulfilorder.php
@@ -23,6 +23,21 @@ if (isset($_GET['salesid'])) {
     $result2->setFetchMode(PDO::FETCH_ASSOC);
     $stockordered = $result2->fetchAll()[0]['quantity'];
 
+    $result3 = $db->query("SELECT purchaseitem.productid
+                    FROM purchaseitem, purchaseorder
+                    WHERE purchaseorder.purchaseid = purchaseitem.purchaseid
+                    AND purchaseorder.status = 'pending'");
+    $result3->setFetchMode(PDO::FETCH_ASSOC);
+    $purchasepend = $result3->fetchAll()[0]['productid'];
+
+    $result4 = $db->query("SELECT salesitem.productid
+                            FROM salesitem, salesorder
+                            WHERE salesorder.sid = salesitem.sid
+                            AND salesorder.status = 'pending'
+                            AND salesorder.sid = {$soId}");
+    $result4->setFetchMode(PDO::FETCH_ASSOC);
+    $salespend = $result4->fetchAll()[0]['productid'];
+
     if ($stockordered > $stockbalance) {
         echo "<script type='text/javascript'>
         alert('CANNOT FULFIL ORDER BECAUSE STOCK LEVEL TOO LOW !!!');
@@ -30,6 +45,13 @@ if (isset($_GET['salesid'])) {
         </script>";
         return false;
 
+        if ($purchasepend == $salespend) {
+            echo "<script type='text/javascript'>
+            alert('PRODUCT ORDERED IS PENDING FULFILLMENT FROM SUPPLIER !!! '<br>' NO NEED TO REORDER AGAIN !!! ');
+            window.location.replace('pendingorders.php');
+          </script>";
+            return false;
+        } else return false;
     } else if ($stockordered <= $stockbalance) {
         try {
             $sql = "UPDATE salesorder
