aadd0b5c0fe98452ab90e1c2701904fb1ad19d31
akpabioignatius <1414987@rgu.ac.uk>
2016-05-01 23:19:36 +0100

    update stock level

--- updatefulfilorder.php
+++ updatefulfilorder.php
@@ -2,14 +2,15 @@
 include('connection.php');
 
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_STRINGIFY_FETCHES => false, PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL));
 
 if (isset($_GET['salesid'])) {
     $soId = $_GET['salesid'];
     $sqlselect = "SELECT * FROM salesorder WHERE sid = $soId";
     $getResult = mysqli_query($db, $sqlselect);
 
-    $stockbalance = $db->query("SELECT stocklevel.stockbalance
+    $stockbalance = $db->query("SELECT stocklevel.stockbalance AS INTEGER
                 FROM stocklevel, salesitem, salesorder
                 WHERE stocklevel.productid = salesitem.productid
                 AND salesitem.sid = salesorder.sid
@@ -18,7 +19,7 @@ if (isset($_GET['salesid'])) {
     echo $stockbalance->fetchAll()[0]['stockbalance'];
     echo "<br>";
 
-    $stockordered = $db->query("SELECT quantity
+    $stockordered = $db->query("SELECT quantity AS INTEGER
                 FROM salesitem
                 WHERE sid = {$soId}");
     $stockordered->setFetchMode(PDO::FETCH_ASSOC);
