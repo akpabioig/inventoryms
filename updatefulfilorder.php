c512bbd69abb3c29d65c4a21ef8f1461da6446cb
akpabioignatius <1414987@rgu.ac.uk>
2016-05-02 00:53:26 +0100

    update stock level

--- updatefulfilorder.php
+++ updatefulfilorder.php
@@ -50,11 +50,13 @@ if (isset($_GET['salesid'])) {
                 alert('PRODUCT BEING ORDER IS AWAITING FULFILLMENT FROM SUPPLIER !!! '<BR>' ***** DO NOT REORDER **** ');
                 window.location.replace('pendingorders.php');
                 </script>";
+                return false;
             } else if ($purchasepend !== $salespend) {
                 echo "<script type='text/javascript'>
                 alert(' REORDER STOCK !!! ');
                 window.location.replace('pendingorders.php');
             </script>";
+                return false;
             }
         }
 
