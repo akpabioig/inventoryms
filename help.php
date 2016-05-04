633d5df652530406bff1063beafc21258094eeca
akpabioignatius <1414987@rgu.ac.uk>
2016-05-04 03:55:13 +0100

    update stock level

---
+++ help.php
@@ -0,0 +1,58 @@
+<?php
+/*session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+}*/
+
+include('connection.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> HELP </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styling.css"/>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="scripting.js"></script>
+</head>
+<body>
+<nav class="w3-sidenav w3-black" style="width:102px">
+    <a class="w3-padding-16" href="homepage.php"><i class="fa fa-home w3-xxlarge"></i> <br>HOME </a>
+    <a class="w3-padding-16" href="addproduct.php"><i class="fa fa-plus-square w3-xlarge"></i> <br>ADD PRODUCT</a>
+    <a class="w3-padding-16" href="addcustomer.php"><i class="fa fa-users w3-xlarge"></i> <br>ADD CUSTOMER</a>
+    <a class="w3-padding-16" href="newsales.php"><i class="fa fa-dollar w3-xlarge"></i><br>SALES ORDER</a>
+    <a class="w3-padding-16" href="newpurchase.php"><i class="fa fa-shopping-cart w3-xlarge"></i>PURCHASE ORDER</a>
+    <a class="w3-padding-16" href="addsupplier.php"><i class="fa fa-truck w3-xlarge"></i><br>ADD SUPPLIER</a>
+    <a class="w3-padding-16" href="location.php"><i class="fa fa-map-marker w3-xlarge"></i><br>LOCATION</a>
+</nav>
+<div style="margin-left:70px">
+    <header>
+        <h1> RGU Oil Services </h1>
+        <h2> Inventory Management Made Easy </h2>
+        <button id="logout"><a href="logout.php">LOG-OUT</a></button>
+    </header>
+    <section>
+        <h3>HELP</h3>
+        <img src="images/help.png" id="helpicon" style{height="250" width="200" }/>
+        <a>
+            <video width="320" height="240" autoplay>
+                <source src="Videos/introduction.mov" type="video/mov">
+                Your browser does not support the video tag.
+            </video>
+        </a>
+        <h2>HELP</h2>
+        <p>WELCOME TO THE HELP PAGE OF THE RGU IMS</p>
+        <p>PLAY ANY OF VIDEOS TO VIEW THE HELP WITH VOICE OVER</p>
+        <div id="form1">
+
+        </div>
+    </section>
+</div>
+<footer>
+    <p>&copy; Akpabio Ignatius, 2016</p>
+</footer>
+</body>
+</html>
