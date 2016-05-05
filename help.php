34922d92b46a0e6fb71b5bbd401aef3384c518c3
akpabioignatius <1414987@rgu.ac.uk>
2016-05-05 01:20:01 +0100

    update stock level

--- help.php
+++ help.php
@@ -17,6 +17,49 @@ include('connection.php');
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
     <script type="text/javascript" src="scripting.js"></script>
+    <style>
+        /* The Modal (background) */
+        .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            padding-top: 100px; /* Location of the box */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0, 0, 0); /* Fallback color */
+            background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+        }
+
+        /* Modal Content */
+        .modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+        }
+
+        /* The Close Button */
+        .close, .close1, .close2, .close3, .close4,
+        .close5, .close6, .close7, .close8 {
+            float: right;
+            font-size: 30px;
+            font-weight: bold;
+            color: red;
+        }
+
+        .close, .close1, .close2, .close3, .close4,
+        .close5, .close6, .close7, .close8 :hover,
+        .close, .close1, .close2, .close3, .close4,
+        .close5, .close6, .close7, .close8 :focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
 <nav class="w3-sidenav w3-black" style="width:102px">
@@ -47,7 +90,7 @@ include('connection.php');
         <div id="form4">
             <table>
                 <tr>
-                    <td> PRODUCT HELP</td>
+                    <td id="producthelp"> PRODUCT HELP</td>
                     <td> CUSTOMER HELP</td>
                     <td> SUPPLIER HELP</td>
                 </tr>
@@ -64,42 +107,52 @@ include('connection.php');
                     </td>
                     <td>
                         <iframe id=intro width="350" height="300" allowfullscreen="allowfullscreen"
-                                src="https://www.youtube.com/embed/?">
+                                src="https://www.youtube.com/embed/bH7vvv1ct84?">
                         </iframe>
                     </td>
                 </tr>
             </table>
         </div>
     </section>
-
 </div>
 <footer>
     <p>&copy; Akpabio Ignatius, 2016</p>
 </footer>
+<div id="myModal" class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+        <span class="close">x</span>
+        <header>
+            <h1> PRODUCT HELP FAQ's</h1>
+        </header>
+        <body>
+        <ol>
+            <li> What is Initial Stock Price ?</li>
+            <p> - Initial Stock Price is the price at which the product is to be bought from the supplier</p>
+            <li> How to get the product Report ?</li>
+            <p> - The Product report can be found in the reports section of the system.
+                The report icon is on the shortcut pane located at the top right of the application's homepage</p>
+        </ol>
+        </body>
+    </div>
+</div>
 <script>
-    var modal17 = document.getElementById('myModal17');
-
-    // Get the button that opens the modal
-    var btn17 = document.getElementById("y2");
-
-    // Get the <span> element that closes the modal
-    var span17 = document.getElementsByClassName("close17")[0];
-
-    // When the user clicks on the button, open the modal
-    btn17.onclick = function () {
-        modal17.style.display = "block";
+    // PRODUCT HELP
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("producthelp");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function () {
+        modal.style.display = "block";
     }
-
-    // When the user clicks on <span> (x), close the modal
-    span17.onclick = function () {
-        modal17.style.display = "none";
+    span.onclick = function () {
+        modal.style.display = "none";
     }
-    // When the user clicks anywhere outside of the modal, close it
-    modal17.onclick = function (event17) {
-        if (event17.target == modal17) {
-            modal17.style.display = "none";
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
-    }
+        }
+
 </script>
 </body>
 </html>
