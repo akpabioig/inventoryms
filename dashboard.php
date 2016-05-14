56b5d67ef93808ecb31055c0d4c7c5492e52440d
akpabioignatius <1414987@rgu.ac.uk>
2016-05-14 20:57:58 +0100

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -342,51 +342,7 @@
                 </div>
                 <!--End Donut Example-->
             </div>
-            <div class="col-lg-4">
-                <!-- Chat Panel Example-->
-                <div class="chat-panel panel panel-primary">
-                    <div class="panel-heading">
-                        <i class="fa fa-comments fa-fw"></i>
-                        Chat
-                        <div class="btn-group pull-right">
-                            <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
-                                <i class="fa fa-chevron-down"></i>
-                            </button>
-                            <ul class="dropdown-menu slidedown">
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-refresh fa-fw"></i>Refresh
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-check-circle fa-fw"></i>Available
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-times fa-fw"></i>Busy
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-clock-o fa-fw"></i>Away
-                                    </a>
-                                </li>
-                                <li class="divider"></li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-sign-out fa-fw"></i>Sign Out
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
 
-
-                </div>
-                <!--End Chat Panel Example-->
-            </div>
         </div>
 
 
