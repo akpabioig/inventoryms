d9f87ee7c7c1a04e2baad654f7514debc10a043b
akpabioignatius <1414987@rgu.ac.uk>
2016-05-14 18:49:19 +0100

    newSales

--- newsales.php
+++ newsales.php
@@ -48,7 +48,7 @@ include('connection.php');
         <button id="logout"><a href="logout.php">LOG-OUT</a></button>
     </header>
     <section>
-        <h3>NEW SALES ORDER 5</h3>
+        <h3>NEW SALES ORDER 12</h3>
         <img src= "images/sales.jpg" style{height="250" width="200"}/>
         <h2>New Sales Order Details</h2>
         <p>Insert the details of the sale to be made to a registered customer </p>
@@ -187,7 +187,8 @@ include('connection.php');
     $(document).ready(function(){
         $("#product").change(function () {
 
-            alert(this.value);
+            //alert(this.value);
+            document.getElementById('costcust').innerHTML = this.value;
             //$("#costcust").val(price);
             //$("#itemcustl").val(locid);
 
