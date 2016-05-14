e4075153469c47f28ef39f97956f9c4a0af4da04
akpabioignatius <1414987@rgu.ac.uk>
2016-05-14 18:40:07 +0100

    update stock level

--- newsales.php
+++ newsales.php
@@ -48,7 +48,7 @@ include('connection.php');
         <button id="logout"><a href="logout.php">LOG-OUT</a></button>
     </header>
     <section>
-        <h3>NEW SALES ORDER 1</h3>
+        <h3>NEW SALES ORDER 0</h3>
         <img src= "images/sales.jpg" style{height="250" width="200"}/>
         <h2>New Sales Order Details</h2>
         <p>Insert the details of the sale to be made to a registered customer </p>
