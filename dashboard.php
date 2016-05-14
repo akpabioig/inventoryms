b00078838c054e1b07e8de4c5e201d5179ffbc02
akpabioignatius <1414987@rgu.ac.uk>
2016-05-14 22:40:27 +0100

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -135,7 +135,7 @@
                 </div>
                 <!--End area chart example -->
                 <!--Simple table example -->
-                <div class="panel panel-primary">
+                <div class="panel-body">
                     <div class="col-lg-6">
                         <!--  Bar Chart -->
                         <div class="panel panel-default">
@@ -146,94 +146,6 @@
                                 <div id="morris-bar-chart"></div>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <ul class="chat">
-                                    <li class="left clearfix">
-                                    <span class="chat-img pull-left">
-                                        <img src="http://placehold.it/50/55C1E7/fff" alt="User Avatar"
-                                             class="img-circle"/>
-                                    </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">Jack Sparrow</strong>
-                                                <small class="pull-right text-muted">
-                                                    <i class="fa fa-clock-o fa-fw"></i>12 mins ago
-                                                </small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-                                                bibendum
-                                                ornare dolor, quis ullamcorper ligula sodales.
-                                            </p>
-                                        </div>
-                                    </li>
-                                    <li class="right clearfix">
-                                    <span class="chat-img pull-right">
-                                        <img src="http://placehold.it/50/FA6F57/fff" alt="User Avatar"
-                                             class="img-circle"/>
-                                    </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <small class=" text-muted">
-                                                    <i class="fa fa-clock-o fa-fw"></i>13 mins ago
-                                                </small>
-                                                <strong class="pull-right primary-font">Bhaumik Patel</strong>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-                                                bibendum
-                                                ornare dolor, quis ullamcorper ligula sodales.
-                                            </p>
-                                        </div>
-                                    </li>
-                                    <li class="left clearfix">
-                                    <span class="chat-img pull-left">
-                                        <img src="http://placehold.it/50/55C1E7/fff" alt="User Avatar"
-                                             class="img-circle"/>
-                                    </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <strong class="primary-font">Jack Sparrow</strong>
-                                                <small class="pull-right text-muted">
-                                                    <i class="fa fa-clock-o fa-fw"></i>14 mins ago
-                                                </small>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-                                                bibendum
-                                                ornare dolor, quis ullamcorper ligula sodales.
-                                            </p>
-                                        </div>
-                                    </li>
-                                    <li class="right clearfix">
-                                    <span class="chat-img pull-right">
-                                        <img src="http://placehold.it/50/FA6F57/fff" alt="User Avatar"
-                                             class="img-circle"/>
-                                    </span>
-                                        <div class="chat-body clearfix">
-                                            <div class="header">
-                                                <small class=" text-muted">
-                                                    <i class="fa fa-clock-o fa-fw"></i>15 mins ago
-                                                </small>
-                                                <strong class="pull-right primary-font">Bhaumik Patel</strong>
-                                            </div>
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-                                                bibendum
-                                                ornare dolor, quis ullamcorper ligula sodales.
-                                            </p>
-                                        </div>
-                                    </li>
-                                </ul>
-
-                            </div>
-
-                        </div>
-                        <!-- /.row -->
                     </div>
                     <!-- /.panel-body -->
                 </div>
