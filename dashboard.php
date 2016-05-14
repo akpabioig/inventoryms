aa1b6b99c46b6ebcf938d47341177532ca3b6530
akpabioignatius <1414987@rgu.ac.uk>
2016-05-14 22:45:00 +0100

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -131,14 +131,42 @@
                     <div class="panel-body">
                         <div id="morris-area-chart"></div>
                     </div>
-
                 </div>
+
+
                 <!--End area chart example -->
                 <!--Simple table example -->
+                <div class="row">
+                    <div class="col-lg-8">
+                        <!--Area chart example -->
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <i class="fa fa-bar-chart-o fa-fw"></i>Bar Chart Example
+                                <div class="pull-right">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-default btn-xs dropdown-toggle"
+                                                data-toggle="dropdown">
+                                            Actions
+                                            <span class="caret"></span>
+                                        </button>
+                                        <ul class="dropdown-menu pull-right" role="menu">
+                                            <li><a href="#">Action</a>
+                                            </li>
+                                            <li><a href="#">Another action</a>
+                                            </li>
+                                            <li><a href="#">Something else here</a>
+                                            </li>
+                                            <li class="divider"></li>
+                                            <li><a href="#">Separated link</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                 <div class="panel-body">
                     <div id="morris-bar-chart"></div>
                 </div>
-
+                        </div>
             </div>
 
             <div class="col-lg-4">
