9123cc9546f2623b44fb84d71ba4f19ea10ff57b
akpabioignatius <1414987@rgu.ac.uk>
2016-05-15 22:18:26 +0100

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -101,48 +101,18 @@
             </div>
 
             <div class="row">
-            <div class="col-lg-8">
-                <!--Area chart example -->
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <i class="fa fa-bar-chart-o fa-fw"></i>Area Chart Example
-                        <div class="pull-right">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-default btn-xs dropdown-toggle"
-                                        data-toggle="dropdown">
-                                    Actions
-                                    <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu pull-right" role="menu">
-                                    <li><a href="#">Action</a>
-                                    </li>
-                                    <li><a href="#">Another action</a>
-                                    </li>
-                                    <li><a href="#">Something else here</a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a>
-                                    </li>
-                                </ul>
-                            </div>
+                <div class="col-lg-6">
+                    <!--  Line Chart -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Line Chart Example
+                        </div>
+                        <div class="panel-body">
+                            <div id="morris-line-chart"></div>
                         </div>
                     </div>
-
-                    <div class="panel-body">
-                        <div id="morris-area-chart"></div>
-                    </div>
-
+                    <!--  End Line Chart -->
                 </div>
-
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Bar Chart Example
-                    </div>
-                <div class="panel-body">
-                    <div id="morris-bar-chart"></div>
-                </div>
-            </div>
-            </div>
 
                 <div class="col-lg-4">
                 <div class="panel panel-primary">
