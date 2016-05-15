11c48e709297e9398f729f55363be6252297d450
Ajibola <jibolso@gmail.com>
2016-05-16 00:54:23 +0100

    NewSale Edit

--- newsales.php
+++ newsales.php
@@ -208,7 +208,7 @@ include('connection.php');
 
         $("#quancust").change(function(){
             $("#minitotal").val(parseInt(parseInt(this.value)* parseInt(adjustedFigure)));
-            $("#totunitcust").val(document.getElementById('quancust').value);
+            $("#totunitcust").val(this.value);
         })
 
         $("#taxcust").change(function(){
