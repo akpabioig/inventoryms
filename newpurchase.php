adb4385a27a95459397ca12295c8c04b003f87d2
akpabioignatius <1414987@rgu.ac.uk>
2016-05-16 18:26:04 +0100

    dashboard

--- newpurchase.php
+++ newpurchase.php
@@ -190,7 +190,10 @@ include('connection.php');
         $("#tax").change(function () {
             $("#itemtotal").val(parseInt(parseInt(document.getElementById('itemtotal').value) + (parseInt(this.value) / 100 * parseInt(document.getElementById('itemtotal').value))));
             $("#totalcost").val(document.getElementById('itemtotal').value);
+            $("#subtotal").val(document.getElementById('totalcost').value) - (parseInt('tax').value) / 100 * parseInt(document.getElementById('itemtotal').value)
+            )
         })
+
     });
 </script>
 </html>
