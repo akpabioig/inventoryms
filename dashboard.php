9ad667271d4a88a9ca35e73653274d28bedf1546
Ajibola <jibolso@gmail.com>
2016-05-16 23:27:09 +0100

    dashboard: dynamic population

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,7 @@ include('connection.php');
 
 $query1 = "select * from dashboard_totals";
 $result1 = mysqli_query($db, $query1);
-while ($count1 = $result1->fetch_array()) {
+$count1 = $result1->fetch_array();
     echo $count1[0] + '\n';
     echo $count1[1] + '\n';
     echo $count1[2] + '\n';
@@ -17,6 +17,6 @@ while ($count1 = $result1->fetch_array()) {
     echo $count1[4] + '\n';
     echo $count1[5] + '\n';
     echo $count1[6] + '\n';
-}
+
 
 ?>
