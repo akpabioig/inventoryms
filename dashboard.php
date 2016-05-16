a3ec05263cb0024845f83dc8f5034a2de2914257
Ajibola <jibolso@gmail.com>
2016-05-17 00:01:09 +0100

    dashboard: dynamic population

--- dashboard.php
+++ dashboard.php
@@ -1,18 +1,12 @@
-<?php
-session_start();
-if (!isset($_SESSION['user'])) {
-    header("Location: login.php");
-}
-?>
+
 <?php
 include('connection.php');
-
+$list = array();
 $query1 = "select * from dashboard_totals";
 $result1 = mysqli_query($db, $query1);
-while($count1 = $result1->fetch_array()){
-    echo $count1[0] .'<br>';
+while($count1 = $result1->fetch_array()) {
+    echo $count1[0] . '<br>';
+    $list[] = $count1[0];
 }
-
-
-
+echo $list[0];
 ?>
