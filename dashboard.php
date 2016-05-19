5bf51587e0f52c58413ab1fbbeb930e802f8ddf1
Ajibola <jibolso@gmail.com>
2016-05-19 23:06:29 +0100

    dashboard patch

--- dashboard.php
+++ dashboard.php
@@ -2,6 +2,14 @@
 session_start();
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
+} else{
+    include('connection.php');
+    $list = array();
+    $query1 = "select * from dashboard_totals";
+    $result1 = mysqli_query($db, $query1);
+    while($count1 = $result1->fetch_array()) {
+    $list[] = $count1[0];
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -21,7 +29,7 @@ if (!isset($_SESSION['user'])) {
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 </head>
 
-<body>
+<?php echo "<body onload='totals($list[0], $list[1], $list[2], $list[3], $list[4], $list[5], $list[6], $list[7]); return false;'>"; ?>
 <nav class="w3-sidenav w3-black" style="width:102px"> <!-- -->
     <a class="w3-padding-16" href="index.php"><i class="fa fa-home w3-xxlarge"></i> <br>HOME </a>
     <a class="w3-padding-16" href="addproduct.php"><i class="fa fa-plus-square w3-xlarge"></i> <br>ADD PRODUCT</a>
@@ -41,7 +49,7 @@ if (!isset($_SESSION['user'])) {
         <!--  wrapper -->
         <div id="wrapper">
 
-            <!--  page-wrapper uiio-->
+            <!--  page-wrapper -->
 
             <div class="row">
                 <!-- Page Header -->
@@ -142,47 +150,19 @@ if (!isset($_SESSION['user'])) {
 
                         <div class="panel-body">
                             <div class="list-group">
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-comment fa-fw"></i>New Comment
-                                    <span class="pull-right text-muted small"><em>4 minutes ago</em>
+                                <?php
+                                $queryNotifications = "select notification from notifications order by id desc LIMIT 10";
+                                $resultNotifications = mysqli_query($db, $queryNotifications);
+                                while($countNotifications = $resultNotifications->fetch_array()) {
+                                    echo "
+                                    <a href=\"#\" class=\"list-group-item\">
+                                    <i class=\"fa fa-info-circle\"></i> {$countNotifications['notification']}
+                                    <span class=\"pull-right text-muted small\">
                                     </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-twitter fa-fw"></i>3 New Followers
-                                    <span class="pull-right text-muted small"><em>12 minutes ago</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-envelope fa-fw"></i>Message Sent
-                                    <span class="pull-right text-muted small"><em>27 minutes ago</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-tasks fa-fw"></i>New Task
-                                    <span class="pull-right text-muted small"><em>43 minutes ago</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-upload fa-fw"></i>Server Rebooted
-                                    <span class="pull-right text-muted small"><em>11:32 AM</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-bolt fa-fw"></i>Server Crashed!
-                                    <span class="pull-right text-muted small"><em>11:13 AM</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-warning fa-fw"></i>Server Not Responding
-                                    <span class="pull-right text-muted small"><em>10:57 AM</em>
-                                    </span>
-                                </a>
-                                <a href="#" class="list-group-item">
-                                    <i class="fa fa-shopping-cart fa-fw"></i>New Order Placed
-                                    <span class="pull-right text-muted small"><em>9:49 AM</em>
-                                    </span>
-                                </a>
-
+                                    </a>
+                                    ";
+                                }
+                                ?>
                             </div>
                             <!-- /.list-group -->
                             <a href="#" class="btn btn-default btn-block">View All Alerts</a>
@@ -236,9 +216,17 @@ if (!isset($_SESSION['user'])) {
         document.getElementById('totalSalesInEarnings').innerHTML = num5;
         document.getElementById('totalPurchaseExpenses').innerHTML = num6;
         document.getElementById('pl').innerHTML = num7;
+
+        console.log('test');
+        console.log(num1);
+        console.log(num2);
+        console.log(num3);
+        console.log(num4);
+        console.log(num5);
+        console.log(num6);
+        console.log(num7);
         //barchart.setData([{ "y": "2014", "a": 100 },{ "y": "2015", "a": 24}]);
     }
-
 
     $(document).ready(function(){
         $.ajax({
@@ -253,28 +241,29 @@ if (!isset($_SESSION['user'])) {
                 yValues.push(height);
                 xValues.push(dataSet[i].slice([1]));
             }
+            console.log('dataSet size: ' + dataSet.length);
             if(dataSet.length-1 == 1){
-     barchart.setData([{"y": xValues[0], "a": yValues[0]}]);
-     }else if(dataSet.length-1 == 2){
-     barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]}]);
-     }else if(dataSet.length-1 == 3){
-     barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
-     {"y": xValues[2], "a": yValues[2]}]);
-     }else if(dataSet.length-1 == 4){
-     barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
-     {"y": xValues[2], "a": yValues[2]}, {"y": xValues[3], "a": yValues[3]}]);
-     }else if(dataSet.length-1 == 5){
-     barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
-     {"y": xValues[2], "a": yValues[2]}, {"y": xValues[3], "a": yValues[3]}, {"y": xValues[4], "a": yValues[4]}]);
-     }else if(dataSet.length-1 == 6){
-     barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
-     {"y": xValues[2], "a": yValues[2]}, {"y": xValues[3], "a": yValues[3]}, {"y": xValues[4], "a": yValues[4]}
-     , {"y": xValues[5], "a": yValues[5]}]);
-     }else if(dataSet.length-1 == 7){
-     barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
-     {"y": xValues[2], "a": yValues[2]}, {"y": xValues[3], "a": yValues[3]}, {"y": xValues[4], "a": yValues[4]}
-     , {"y": xValues[5], "a": yValues[5]}, {"y": xValues[6], "a": yValues[6]}]);
-     }
+                barchart.setData([{"y": xValues[0], "a": yValues[0]}]);
+            }else if(dataSet.length-1 == 2){
+                barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]}]);
+            }else if(dataSet.length-1 == 3){
+                barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
+                    {"y": xValues[2], "a": yValues[2]}]);
+            }else if(dataSet.length-1 == 4){
+                barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
+                    {"y": xValues[2], "a": yValues[2]}, {"y": xValues[3], "a": yValues[3]}]);
+            }else if(dataSet.length-1 == 5){
+                barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
+                    {"y": xValues[2], "a": yValues[2]}, {"y": xValues[3], "a": yValues[3]}, {"y": xValues[4], "a": yValues[4]}]);
+            }else if(dataSet.length-1 == 6){
+                barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
+                    {"y": xValues[2], "a": yValues[2]}, {"y": xValues[3], "a": yValues[3]}, {"y": xValues[4], "a": yValues[4]}
+                    , {"y": xValues[5], "a": yValues[5]}]);
+            }else if(dataSet.length-1 == 7){
+                barchart.setData([{"y": xValues[0], "a": yValues[0]}, {"y": xValues[1], "a": yValues[1]},
+                    {"y": xValues[2], "a": yValues[2]}, {"y": xValues[3], "a": yValues[3]}, {"y": xValues[4], "a": yValues[4]}
+                    , {"y": xValues[5], "a": yValues[5]}, {"y": xValues[6], "a": yValues[6]}]);
+            }
         })
     });
 
@@ -293,10 +282,13 @@ if (!isset($_SESSION['user'])) {
                 periodT.push(ht);
                 salesT.push(lineDataSet[i].slice([2]));
             }
+            console.log(periodT[0]);
+            console.log(salesT[0]);
+            console.log(lineDataSet.length-1);
             var d = new Date();
             var n = d.getFullYear();
             if(lineDataSet.length-1 == 1){
-                linechart.setData([{"period": n+'-0'+periodT[0], "sales": salesT[0]}]);
+
             }else if(lineDataSet.length-1 == 2){
                 linechart.setData([{"period": n+'-0'+periodT[0], "sales": salesT[0]}, {"period": n+'-0'+periodT[1], "sales": salesT[1]}]);
             }else if(lineDataSet.length-1 == 3){
@@ -356,6 +348,19 @@ if (!isset($_SESSION['user'])) {
                     {"period": n+'-0'+periodT[8], "sales": salesT[8]}, {"period": n+'-0'+periodT[9], "sales": salesT[9]},
                     {"period": n+'-0'+periodT[10], "sales": salesT[10]}, {"period": n+'-0'+periodT[11], "sales": salesT[11]}]);
             }
+            //linechart.setData([{"period": n+'-0'+periodT[0], "sales": salesT[0]}, {"period": n+'-0'+periodT[1], "sales": salesT[1]}]);
+            /*if(lineDataSet.length <2){
+                linechart.setData([{"period": n+'-0'+periodT[0], "sales": salesT[0]}]);
+            }else if(lineDataSet.length <3){
+                linechart.setData([{"period": n+'-0'+periodT[0], "sales": salesT[0]}, {"period": n+'-0'+periodT[1], "sales": salesT[1]}]);
+            }else if(lineDataSet.length <4){
+                linechart.setData([{"period": n+'-0'+periodT[0], "sales": salesT[0]}, {"period": n+'-0'+periodT[1], "sales": salesT[1]},
+                    {"period": n+'-0'+periodT[2], "sales": salesT[2]}]);
+            }else if(lineDataSet.length <4){
+                linechart.setData([{"period": n+'-0'+periodT[0], "sales": salesT[0]}, {"period": n+'-0'+periodT[1], "sales": salesT[1]},
+                    {"period": n+'-0'+periodT[2], "sales": salesT[2]}, {"period": n+'-0'+periodT[3], "sales": salesT[3]}]);
+            }*/
+
 
 
         })
