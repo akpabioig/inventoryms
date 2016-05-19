c5ececb293be0da94120a4865858220281170ebf
Ajibola <jibolso@gmail.com>
2016-05-19 21:56:14 +0100

    dashboard stripped for test

--- dashboard.php
+++ dashboard.php
@@ -10,6 +10,13 @@ if (!isset($_SESSION['user'])) {
     while($count1 = $result1->fetch_array()) {
     $list[] = $count1[0];
     }
-    echo $list[0];
+    echo $list[0] + ' \n';
+    echo $list[1] + ' \n';
+    echo $list[2] + ' \n';
+    echo $list[3] + ' \n';
+    echo $list[4] + ' \n';
+    echo $list[5] + ' \n';
+    echo $list[6] + ' \n';
+    echo $list[7];
 }
 
