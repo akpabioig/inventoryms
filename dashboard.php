9183e79049fe957df1806482e97bc3dc874febf0
Ajibola <jibolso@gmail.com>
2016-05-19 22:44:30 +0100

    dashboard totals function

--- dashboard.php
+++ dashboard.php
@@ -246,9 +246,8 @@ if (!isset($_SESSION['user'])) {
             type: 'POST',
         }).done(function (data) {
             chartDataSet = data.match(/^.*((\r\n|\n|\r)|$)/gm);
-            for(var i=0; i<chartDataSet.length; i++){
-                console.log(chartDataSet[i]);
-            }
+            totals(chartDataSet[0], chartDataSet[1], chartDataSet[2], chartDataSet[3], chartDataSet[4], chartDataSet[5],
+                chartDataSet[6], chartDataSet[7]);
 
         })
     });
