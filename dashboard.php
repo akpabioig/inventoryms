09c38a28a0890376cd8d65e01016f6789e69f82a
Ajibola <jibolso@gmail.com>
2016-05-19 19:40:25 +0100

    notification bit

--- dashboard.php
+++ dashboard.php
@@ -153,6 +153,7 @@ if (!isset($_SESSION['user'])) {
                         <div class="panel-body">
                             <div class="list-group">
                                 <?php
+                                /*
                                 $queryNotifications = "select notification from notifications order by id desc LIMIT 10";
                                 $resultNotifications = mysqli_query($db, $queryNotifications);
                                 while($countNotifications = $resultNotifications->fetch_array()) {
@@ -163,7 +164,7 @@ if (!isset($_SESSION['user'])) {
                                     </span>
                                     </a>
                                     ";
-                                }
+                                }*/
                                 ?>
                             </div>
                             <!-- /.list-group -->
