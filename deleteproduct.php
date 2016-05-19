506b5c475a62c6f5cf8865798e771e9b8e6f6802
Ajibola <jibolso@gmail.com>
2016-05-19 19:33:54 +0100

    notification bit

--- deleteproduct.php
+++ deleteproduct.php
@@ -1,25 +1,22 @@
 <?php
-$db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
+include('connection.php');
+$db1 = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 if (isset($_GET['prodid'])) {
     $prodId = $_GET['prodid'];
 
+    $q = "SELECT productname from addproduct where productid = {$prodId} LIMIT 1";
+    $qResult = mysqli_query($db1, $q);
+
     try {
         $sql = "DELETE FROM addproduct
                     WHERE productid = {$prodId}";
-        $sth = $db->query($sql);
-
-
-        $query1 = "select * from dashboard_totals";
-        $result1 = mysqli_query($db, $query1);
-
-        $sql4 = "SELECT productname from addproduct where productid = {$prodId}";
-        $sth4 = $db->query($sth4);
+        $sth = $db1->query($sql);
 
         $sql3 = "INSERT INTO notifications (notification)
-                    VALUES ('Product: {$sth4} was deleted')";
-        $sth3 = $db->query($sql3);
+                    VALUES ('Product: {$qResult} was deleted')";
+        $sth3 = $db1->query($sql3);
 
     } catch (PDOException $e) {
         echo $e->getMessage();
