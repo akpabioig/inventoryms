c0552d8dc2b8bf60318c42c8a54882bbbea6e89f
Ajibola <jibolso@gmail.com>
2016-05-19 02:49:27 +0100

    dashboard changes

--- dashboard.php
+++ dashboard.php
@@ -289,7 +289,7 @@ if (!isset($_SESSION['user'])) {
             for(i=0; i<lineDataSet.length; i++){
                 //console.log(lineDataSet[i]);
                 var ld = lineDataSet[i].slice([1,lineDataSet[i].length]);
-                console.log('number 2:' + lineDataSet[i][2]);
+                console.log('number 2:' + lineDataSet[i].slice([2, lineDataSet[i].length]));
                 var ht = (''+ld)[0];
                 periodT.push(ht);
                 salesT.push(lineDataSet[i].slice([2]));
