b12618dadceb89d225f3cac2e0e6c7c6f00aae39
Ajibola <jibolso@gmail.com>
2016-05-19 23:55:05 +0100

    dashboard delete notification

--- deleteproduct.php
+++ deleteproduct.php
@@ -6,16 +6,28 @@
  * Time: 12:30 PM
  *
  */
+
+$prodId = $_GET['prodid'];
+include ('connection.php');
+$queryNotifications = "select productname from addproduct where productid = {$prodId} LIMIT 1";
+$resultNotifications = mysqli_query($db, $queryNotifications);
+while($countNotifications = $resultNotifications->fetch_array()) {
+    $qResult = $countNotifications[0];
+}
+
 $db = new PDO('mysql:host=us-cdbr-azure-southcentral-e.cloudapp.net;dbname=inventoryms;charset=utf8mb4', 'bee886bc8793e7', '362289e3', array(PDO::ATTR_EMULATE_PREPARES => false,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 if (isset($_GET['prodid'])) {
-    $prodId = $_GET['prodid'];
 
     try {
         $sql = "DELETE FROM addproduct
                     WHERE productid = {$prodId}";
         $sth = $db->query($sql);
+
+        $sql3 = "INSERT INTO notifications (notification)
+                            VALUES ('Product: {$qResult} was deleted')";
+        $sth3 = $db1->query($sql3);
 
     } catch (PDOException $e) {
         echo $e->getMessage();
