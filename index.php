2ea4756472e828080c40a4f5d66f067ed839032b
akpabioignatius <1414987@rgu.ac.uk>
2016-10-24 23:22:42 +0100

    Home

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('connection.php');
 <!DOCTYPE html>
 <html>
 <head>
-    <title> HOME - PAGE </title>
+    <title> HOME PAGE </title>
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="HandheldFriendly" content="true">
